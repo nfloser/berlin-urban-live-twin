2747d128a979d84035ea184eeb5971458f047442
nfloser <102595695+nfloser@users.noreply.github.com>
2026-09-14 11:39:42 +0200

    Test PROV-O VBB lineage

--- agents/transit-agent/tests/test_rdf_mapper.py
+++ agents/transit-agent/tests/test_rdf_mapper.py
@@ -2,13 +2,15 @@
 
 from datetime import datetime, timezone
 
-from rdflib import RDF, XSD, Literal, URIRef
+from rdflib import RDF, XSD, Literal, Namespace, URIRef
 
 from app.models import TransitSnapshot
 from app.rdf_mapper import CITY, transit_to_graph
 
+PROV = Namespace("http://www.w3.org/ns/prov#")
 
-def test_transit_snapshot_to_graph_creates_summary_triples() -> None:
+
+def test_transit_snapshot_to_graph_creates_summary_triples_and_lineage() -> None:
     snapshot = TransitSnapshot(
         generated_at=datetime(2026, 9, 14, 8, 0, tzinfo=timezone.utc),
         total_trip_updates=200,
@@ -23,3 +25,4 @@ def test_transit_snapshot_to_graph_creates_summary_triples() -> None:
     assert (subject, CITY.totalTripUpdates, Literal(200, datatype=XSD.integer)) in graph
     assert (subject, CITY.delayedTripUpdates, Literal(40, datatype=XSD.integer)) in graph
     assert (subject, CITY.delayedShare, Literal(0.2, datatype=XSD.double)) in graph
+    assert (subject, PROV.wasDerivedFrom, URIRef("https://production.gtfsrt.vbb.de/data")) in graph
